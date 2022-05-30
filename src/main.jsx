2a1765965ad56b1d51095893296572aa904ce13d
chenweilun <chenwl@shinemo.com>
2022-05-30 14:23:10 +0800

    feat: concurrent模式改为legacy模式

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// concurrent 模式
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+// "legacy" 模式
+import { render } from 'react-dom';
+import './index.css';
+import App from './App';
+
+render(<App />, document.getElementById('root'))
